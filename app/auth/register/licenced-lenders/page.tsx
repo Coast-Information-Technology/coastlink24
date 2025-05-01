1bc3e04bc639cd4cb997fe4f79153682906fa91b
Toluwalope-Coast <rodneytoluwalope@gmail.com>
2025-05-01 10:41:29 +0100

    👌: Updated

--- app/auth/register/licenced-lenders/page.tsx
+++ app/auth/register/licenced-lenders/page.tsx
@@ -683,15 +683,13 @@ const SignUpPage: React.FC = () => {
                   >
                     Date of Birth
                   </label>
-                  <div className="mt-1">
-                    <input
-                      className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                      required
-                      type="date"
-                      name="dob"
-                      id="dob"
-                    />
-                  </div>
+                  <input
+                    className="appearance-none block w-full p-3 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required
+                    type="date"
+                    name="dob"
+                    id="dob"
+                  />
                 </div>
 
                 {/* Country Selection Field */}
